b68f0e2f5b3a70cfb35a42a83d8a0967163b0ad9
imzuddin <imzuddin@gmail.com>
2024-05-26 11:36:40 -0400

    delete: removed unused favicon

--- safe_harbour/tailwind.config.ts
+++ safe_harbour/tailwind.config.ts
@@ -7,14 +7,16 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    extend: { 
+      colors: {
+        blue: {
+          400: '#2589FE',
+          500: '#0070F3',
+          600: '#2F6FEB',
+        },  
+      }, 
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 };
 export default config;
